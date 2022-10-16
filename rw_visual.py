afbabccba30df2a6f1fddf80bc893aa8c219621f
Chris Britton <chrisbritton790@gmail.com>
2022-10-16 14:16:50 -0700

    applied colormap

--- rw_visual.py
+++ rw_visual.py
@@ -6,13 +6,18 @@ from spy_random_walk import SpyRandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = SpyRandomWalk()
+    rw = RandomWalk()
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
